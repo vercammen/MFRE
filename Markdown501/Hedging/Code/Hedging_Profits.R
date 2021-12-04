0ee48c0aacdd8443a6e3d5b78a35689a046e4d80
Vercammen <james.vercammen@ubc.ca>
2021-12-03 19:49:54 -0800

    Files pushed 8 pm Dec 3 2021

--- Markdown501/Hedging/Code/Hedging_Profits.R
+++ Markdown501/Hedging/Code/Hedging_Profits.R
@@ -3,33 +3,79 @@ graphics.off()
 
 # load packages and data
 pacman::p_load(here, dplyr, ggplot2)
-profits <- readRDS(file = "compare.rds")
+profits <- readRDS(file = here("Data", "compare.rds"))
 head(profits)
 
-profits <- profits %>% mutate(BasisChange = cornBasis3 - cornBasis1) # this could have been added in hedging_simulate.R rather than here.
+# Create histograms
+long_corn <- ggplot(profits, aes(x=long_corn)) +  xlab("Profits, Long in Corn ($/bu)") +
+  geom_histogram(color="black", fill="white", bins = 30) + scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) +
+  theme(axis.title=element_text(size=15))
+long_corn
+
+short_hedge <- ggplot(profits, aes(x=short_hedge)) +  xlab("Profits, Short Hedge ($/bu)") +
+  geom_histogram(color="black", fill="white", bins = 30) + scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5))+
+  theme(axis.title=element_text(size=15))
+short_hedge
+
+crush_hedged <- ggplot(profits, aes(x=crushH)) +  xlab("Normalized Crush Margin, Hedged ($/bu)") +
+  geom_histogram(color="black", fill="white", bins = 30) + scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5))+
+  theme(axis.title=element_text(size=15))
+crush_hedged
+
+crush_nohedged <- ggplot(profits, aes(x=crushNH)) +  xlab("Normalized Crush Margin, No Hedge ($/bu)") +
+  geom_histogram(color="black", fill="white", bins = 30) + scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5))+
+  theme(axis.title=element_text(size=15))
+crush_nohedged
+
+basis_change <- ggplot(profits, aes(x=BasisChange)) +  xlab("Change in Basis over Hedging Period ($/bu)") +
+  geom_histogram(color="black", fill="white", bins = 30) + scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) + theme_classic(base_size = 15)
+basis_change
 
 
-# Currently I manually change the X variable to different names to generate a new histogram.
-# The better alternative would be to have separate code for each histogram.
-# The variables in question are the names of the five histograms in the Images folder.
-# I CAN'T FIX WARNING MESSAGE ABOUT SELECTING BIN SIZE
+# overlay hist
+overlay_deferred <- ggplot(profits) +
+  geom_histogram(aes(x= long_corn), fill = "red", alpha = 0.2) +
+  geom_histogram(aes(x = short_hedge), fill = "blue", alpha = 0.2) + 
+  scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) +
+  xlab("Profits, $/bu") + labs(caption = "red = long spot, blue = short hedge")
+overlay_deferred
 
-ggplot(profits, aes(x=crushH)) + geom_histogram()
-# Change the width of bins
-ggplot(profits, aes(x=crushH)) + 
-  geom_histogram(binwidth=0.1)
-# Change colors
-p<-ggplot(profits, aes(x=crushH)) +  xlab("Deviation from Target Crush Margin, Hedged ($/bu)") +
-  geom_histogram(color="black", fill="white")
-p
+overlay_TCM <- ggplot(profits) +
+  geom_histogram(aes(x= crushNH), fill = "red", alpha = 0.2) +
+  geom_histogram(aes(x = crushH), fill = "blue", alpha = 0.2) + 
+  scale_x_continuous(limits = c(-1.5, 1.5), breaks = c(-1.5, -1, -0.5, 0, 0.5, 1, 1.5)) +
+  xlab("Profits, $/bu") + labs(caption = "red = unhedged crush, blue = hedged crush")
+overlay_TCM 
 
-# Calculate the percentiles which are associated with each histogram.
-# There should also be one of these for each of the five variables 
-# I am currently manually changing the names to generate a new set of percentiles
-# I pasted the results directly into my slides. Better to have a copy of all results here.
-crushH_sorted <- sort(profits$crushH)
-crushH_percentile <- quantile(crushH_sorted, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+# save images - uncomment to run and overwrite files 
+ggsave(long_corn, file = here("Images", "hist_long_corn.png"))
+ggsave(short_hedge, file = here("Images", "hist_short_hedge.png"))
+# ggsave(overlay_deferred, file = here("Images", "overlay_deferred.png"))
+ggsave(crush_hedged, file = here("Images", "hist_crush_hedged.png"))
+ggsave(crush_nohedged, file = here("Images", "hist_crush_nohedged.png"))
+ggsave(basis_change, file = here("Images", "hist_basischange.png"))
+
+
+crushH_percentile <- quantile(profits$crushH, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+crushNH_percentile <- quantile(profits$crushNH, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+basischange_percentile <- quantile(profits$BasisChange, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+longcorn_percentile <- quantile(profits$long_corn, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+shorthedge_percentile <- quantile(profits$short_hedge, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+longhedge_percentile <- quantile(profits$long_hedge, probs = c(0.05,0.1,0.2,0.3,0.4,0.5))
+
 crushH_percentile
+crushNH_percentile
+basischange_percentile
 
- 
+longhedge_percentile
+shorthedge_percentile
+
+perc <- rbind(crushH_percentile, crushNH_percentile, basischange_percentile, longhedge_percentile, shorthedge_percentile)
+
+# rename rows
+rownames(perc) <- c("Crush Hedged", "Crush No Hedged", "Basis Change", "Long Hedge", "Short Hedge")
+perc
+
+
+
 
--- Markdown501/Hedging/Code/Hedging_Summary.R
+++ Markdown501/Hedging/Code/Hedging_Summary.R
@@ -5,29 +5,49 @@ graphics.off()
 pacman::p_load(here, dplyr, ggplot2)
 data <- read.csv(here("Data", "corn_ethanol_data.csv"), header=TRUE, sep=",", stringsAsFactors = FALSE) 
 
-
-#data$Date <- as.Date(data$Date, format = c("%m/%d/%y")) I CAN'T GET THIS METHOD TO WORK!
-data$Date <- as.Date(data$Date, origin="1900-01-01")
+data$Date <- as.Date(data$Week, format = c("%m/%d/%Y")) 
 head(data)
 
 # plot the three spot price data series
 plot_spot_corn <- ggplot(data, aes(x = Date, y = CornSpot)) +  
   geom_line() + 
-  labs(title = "Weekly Corn Spot Price, Iowa", y= "$/bu", x = "Date") + 
+  labs(title = "Weekly Corn Spot Price, Iowa", y= "$/bu", x="") + 
   theme(plot.title = element_text(size=10)) 
-  plot_spot_corn
-  
+#plot_spot_corn
+
 plot_spot_DDG <- ggplot(data, aes(x = Date, y = DDG)) +  
-    geom_line() + 
-    labs(title = "Weekly DDG Spot Price, Iowa", y= "$/Ton", x = "Date") + 
-    theme(plot.title = element_text(size=10)) 
-  plot_spot_DDG
-  
-plot_spot_ethanol <- ggplot(data, aes(x = Date, y = CornSpot)) +  
-    geom_line() + 
-    labs(title = "Weekly Ethanol Spot Price, Iowa", y= "$/Gallon", x = "Date") + 
-    theme(plot.title = element_text(size=10)) 
-plot_spot_ethanol
+  geom_line() + 
+  labs(title = "Weekly DDG Spot Price, Iowa", y= "$/Ton", x="") + 
+  theme(plot.title = element_text(size=10)) 
+#plot_spot_DDG
+
+plot_spot_ethanol <- ggplot(data, aes(x = Date, y = Ethanol)) +  
+  geom_line() + 
+  labs(title = "Weekly Ethanol Spot Price, Iowa", y= "$/Gallon", x="") + 
+  theme(plot.title = element_text(size=10)) 
+#plot_spot_ethanol
+
+# uncomment to overwrite files
+# ggsave(plot_spot_corn, file = here("Images", "CornSpot.png"), dpi = 150)
+# ggsave(plot_spot_DDG, file = here("Images", "DDG.png"), dpi = 150)
+# ggsave(plot_spot_ethanol, file = here("Images", "Ethanol.png"), dpi = 150)
+
+# plot the two basis series
+plot_basis_corn <- ggplot(data, aes(x = Date, y = BasisCorn)) +  
+  geom_line() + 
+  labs(title = "Weekly Corn Basis, Iowa", y= "$/bu", x="") + 
+  theme(plot.title = element_text(size=10)) 
+#plot_basis_corn
+
+plot_basis_ethan <- ggplot(data, aes(x = Date, y = BasisEthan)) +  
+  geom_line() + 
+  labs(title = "Weekly Ethanol Basis, Iowa", y= "$/gallon", x="") + 
+  theme(plot.title = element_text(size=10)) 
+#plot_basis_ethan
+
+# uncomment to overwrite files
+#ggsave(plot_basis_corn, file = here("Images", "BasisCorn.png"), dpi = 150)
+#ggsave(plot_basis_ethan, file = here("Images", "BasisEthanol.png"), dpi = 150)
 
 # construct an ethanol revenue and cost data series (standardized to $/bu) and then plot.
 data <- mutate(data, 
@@ -37,10 +57,13 @@ data <- mutate(data,
 plot_crush <- ggplot(data, aes(x = Date)) + 
   geom_line(aes(y = Revenue, color = "Revenue")) + 
   geom_line(aes(y = Cost, color = "Cost")) + 
-  labs(title = "Revenue and Cost in Ethanol Productions", y = "$/bu", x = "Date") +
+  labs(title = "Revenue and Cost in Ethanol Productions", y = "$/bu", x="") +
   theme(plot.title = element_text(size=10)) +
   scale_color_manual(name = "Variable", values = c("Revenue" = "blue", "Cost" = "red"))
-plot_crush
+#plot_crush
+
+# uncomment to overwrite files
+# ggsave(plot_crush, file = here("Images", "Revenue_Cost.png"), dpi = 150)
 
 # optimal hedge ratio (beta when regression log of spot price on log of futures price)
 data <- data %>%
@@ -49,3 +72,50 @@ data <- data %>%
 
 hedge_ratio <- lm(log_spot ~ log_fut, data = data)
 summary(hedge_ratio)
+
+# Plot the wheat prices and basis for Kansas (end of module material)
+ 
+# read in the wheat data
+prices <- read.csv(here("Data", "wheat_covid.csv"), header=TRUE, sep=",", stringsAsFactors = FALSE)
+prices$Date <- as.Date(prices$Date, format = c("%m/%d/%Y"))
+prices <- prices %>% mutate(BasisMay = Spot - May, BasisDec = Spot - Dec)
+
+plot_prices <- ggplot(prices, aes(x = Date)) +  
+  geom_line(aes(y = May, color = "May Futures")) + 
+  geom_line(aes(y = Dec, color = "Dec Futures")) + 
+  geom_line(aes(y = Spot, color = "Kansas Spot")) +
+  labs(title = "Wheat Spot Price (Kansas) and Wheat Futures: Febuary to April, 2020", y= "$/bu", x="") + 
+  theme(plot.title = element_text(size=10)) + 
+  scale_color_manual(name = "Variable", values = c("May Futures" = "blue", "Dec Futures" = "red", "Kansas Spot" = "black"))
+#plot_prices
+
+plot_basis <- ggplot(prices, aes(x = Date)) +  
+  geom_line(aes(y = BasisMay, color = "May Basis")) + 
+  geom_line(aes(y = BasisDec, color = "Dec Basis")) + 
+  labs(title = "Kansas Wheat Basis (May & Dec Futures): Febuary to April, 2020", y= "$/bu", x="") + 
+  theme(plot.title = element_text(size=10)) + 
+  scale_color_manual(name = "Variable", values = c("May Basis" = "blue", "Dec Basis" = "red"))
+#plot_basis
+
+# uncomment to overwrite files
+# ggsave(plot_prices, file = here("Images", "WheatPrices.png"), dpi = 150)
+# ggsave(plot_basis, file = here("Images", "WheatBasis.png"), dpi = 150)
+
+# calculate hedging profits assuming hedge is created on March 18
+# filter the data for prices beginning March 18
+prices2 <- prices %>% filter(Date > "0020-03-15")
+start_may <- prices2[1,"BasisMay"]
+start_dec <- prices2[1,"BasisDec"]
+prices2 <- prices2 %>% mutate(count = c(1:n()), BasisChMay = BasisMay - start_may, BasisChDec = BasisDec - start_dec)
+
+# graph the change in basis for varying hedging periods ==> short hedging profits
+plot_profits <- ggplot(prices2, aes(x = count)) +  
+  geom_line(aes(y = BasisChMay, color = "May Basis Change")) + 
+  geom_line(aes(y = BasisChDec, color = "Dec Basis Change")) + 
+  labs(title = "Short Wheat Hedging Profits: Days Left in Hedge", y= "$/bu", x="Days Left in Hedge") + 
+  theme(plot.title = element_text(size=10)) + 
+  scale_color_manual(name = "Variable", values = c("May Basis Change" = "blue", "Dec Basis Change" = "red"))
+plot_profits
+
+# uncomment to overwrite files
+#ggsave(plot_profits, file = here("Images", "WheatProfits.png"), dpi = 150)
